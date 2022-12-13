a53937c7509cfed92bc6075d38ecbe256c41f2c1
Alok <alokchaturvedi0145@gmail.com>
2022-12-14 01:54:22 +0530

    commit by alok

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import express from 'express'
 import { configureDb } from './src/config/productconfig.js';
 import ProductRouter from './src/router/ProductRouter.js';
 
+
 const app = express(); 
 app.use(express.json())
+
+
 app.use(ProductRouter)
 configureDb()
+
+
 app.listen(7800,()=>{
     console.log("7800 is running");
 })
